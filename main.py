110bef04fa64359fb4780a8cea81027c48e0412f
Tsvetkov I. <145704472+lanfrod@users.noreply.github.com>
2025-05-21 13:14:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ import socket
 
 class DBClient:
     def __init__(self):
-        self.server_address = ('ru.tuna.am', 21091)
+        self.server_address = ('ru.tuna.am', 23513)
         self.timeout = 0.5
 
     def execute_query(self, query, params=None):
